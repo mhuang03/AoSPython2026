be0d16b642041efd3b943b378dd90ede6cf6342e
Michael Huang <32221378+mhuang03@users.noreply.github.com>
2026-07-08 18:27:53 -0500

    revert day2 debugging

--- day2/debugging.py
+++ day2/debugging.py
@@ -1,26 +1,22 @@
 # Exercise 1
 # import random
+# a = random.randint(1,12)
+# b = random.randint(1,12)
 # i = 0
 # while i < 10:
-#     a = random.randint(1,12)
-#     b = random.randint(1,12)
-#
-#     question = "What is "+str(a)+" x "+str(b)+"? "
-#     # question = f"What is {a} x {b}? "
-#     answer = int(input(question))
-#     if answer == a*b:
-#         print("Well done!")
+#     question = "What is "+a+" x "+b+"? "
+#     answer = input(question)
+#     if answer = a*b
+#         print(Well done!)
 #     else:
 #         print("No.")
-#     i += 1
 
 # Exercise 2
 # Sum of even numbers less than or equal to it
 # number = int(input("Enter an integer: "))
 # i = 1
-# total = 0
-# while i <= number:
-#     if i % 2 == 0:
-#         total = total + i
-#     i += 1
+# while i < number:
+#     if number % 2 = 0:
+#         total = total + number
+#     i + 1
 # print(total)
