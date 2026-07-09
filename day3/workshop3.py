4e0e8086341f9d29bb9f323db9de5482b525f64c
mhuang03 <huangmichael150@gmail.com>
2026-07-09 16:54:28 -0500

    day3 ws

---
+++ day3/workshop3.py
@@ -0,0 +1,30 @@
+# Problem 1: Word Count
+# Write a program that asks the user for some words, then prints each unique word and
+# how many times it appeared.
+# Hint: Use a dictionary to store the counts of each word.
+
+
+
+# Problem 2: Isogram
+# Design a function that accepts a string and returns True if the string is an isogram,
+# and False otherwise. An isogram is a word or phrase without any repeating letters.
+# For example, "hello" is not an isogram, but "world" is.
+# Hint: Use a set to store the letters in the string and check for duplicates.
+
+
+
+# Problem 3: Anagrams
+# Design a function that accepts two strings and returns True if the strings are anagrams,
+# and False otherwise. Anagrams are words or phrases that are formed by rearranging the
+# letters of another word or phrase, using all the original letters exactly once.
+# For example, "listen" and "silent" are anagrams.
+# Hint: You could: 1) use a dictionary to store the counts of each letter in each string.
+#               or 2) sort the letters in each string and compare the sorted strings.
+
+
+
+# Problem 4: Collinear Points
+# Design a function that accepts a list of n tuples, each representing the coordinates
+# of a point in 2D space, and returns True if all the points are collinear, and False otherwise.
+# Two or more points are collinear if they all lie on the same straight line.
+# Hint: Use the slope formula to calculate the slope of the line between points.
